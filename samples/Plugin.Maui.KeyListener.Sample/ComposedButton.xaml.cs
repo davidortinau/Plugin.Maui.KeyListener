c1c112e8250418070d6ee01e8bdc20e2e9fe4636
Shane Neuville <shneuvil@microsoft.com>
2025-04-11 12:32:30 -0500

    - add focus calls

--- samples/Plugin.Maui.KeyListener.Sample/ComposedButton.xaml.cs
+++ samples/Plugin.Maui.KeyListener.Sample/ComposedButton.xaml.cs
@@ -5,9 +5,23 @@ public partial class ComposedButton : ContentPage
 	public ComposedButton()
 	{
 		InitializeComponent();
+		FocusableContentView1.Focused += FocusableContentView1_Focused;
+		this.Loaded += ComposedButton_Loaded;
+		
 	}
 
-	private void VerticalStackLayout_Focused(object sender, FocusEventArgs e)
+	void ComposedButton_Loaded(object sender, EventArgs e)
 	{
+		FocusableContentView1.Focus();
+	}
+
+	void FocusableContentView1_Focused(object sender, FocusEventArgs e)
+	{
+			
+	}
+
+	void Button_Clicked(object sender, EventArgs e)
+	{
+		FocusableContentView1.Focus();
 	}
 }
