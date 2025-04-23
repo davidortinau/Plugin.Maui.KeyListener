7b158b7d6abb0056d2f8fbf98ad300eb851d90cd
Shane Neuville <shneuvil@microsoft.com>
2025-04-23 16:54:40 -0500

    Bump everything to net9.0

--- src/Plugin.Maui.KeyListener/MauiProgramExtension.cs
+++ src/Plugin.Maui.KeyListener/MauiProgramExtension.cs
@@ -11,6 +11,9 @@ public static class MauiProgramExtensions
 			#if IOS || MACCATALYST
 				PageHandler.PlatformViewFactory = (handler) =>
 				{
+					if (handler is not PageHandler pageHandler)
+						return null;
+						
 					var vc = new KeyboardPageViewController(handler.VirtualView, handler.MauiContext);
 					handler.ViewController = vc;
 					return (Microsoft.Maui.Platform.ContentView)vc.View.Subviews[0];
