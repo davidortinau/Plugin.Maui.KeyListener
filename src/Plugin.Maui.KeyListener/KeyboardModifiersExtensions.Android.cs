577e24e86acba15b5999485f9e74eaf94d5b11e3
Jeremy Powell <jeremy@visionaid.com>
2025-06-02 14:23:08 +1200

    Implement initial Android support
    Fixes: #4

---
+++ src/Plugin.Maui.KeyListener/KeyboardModifiersExtensions.Android.cs
@@ -0,0 +1,44 @@
+﻿using Android.Views;
+
+namespace Plugin.Maui.KeyListener;
+
+internal static class KeyboardModifiersExtensions
+{
+    /// <summary>
+    /// Converts KeyboardModifiers to Android.Views.MetaKeyStates.
+    /// </summary>
+    public static MetaKeyStates ToMetaKeyStates(this KeyboardModifiers modifiers)
+    {
+        MetaKeyStates result = MetaKeyStates.None;
+
+        if (modifiers.HasFlag(KeyboardModifiers.Shift))
+            result |= MetaKeyStates.ShiftOn;
+        if (modifiers.HasFlag(KeyboardModifiers.Control))
+            result |= MetaKeyStates.CtrlOn;
+        if (modifiers.HasFlag(KeyboardModifiers.Alt))
+            result |= MetaKeyStates.AltOn;
+        if (modifiers.HasFlag(KeyboardModifiers.Command))
+            result |= MetaKeyStates.MetaOn;
+
+        return result;
+    }
+
+    /// <summary>
+    /// Converts Android.Views.MetaKeyStates to KeyboardModifiers.
+    /// </summary>
+    public static KeyboardModifiers ToKeyboardModifiers(this MetaKeyStates metaState)
+    {
+        KeyboardModifiers result = KeyboardModifiers.None;
+
+        if (metaState.HasFlag(MetaKeyStates.ShiftOn))
+            result |= KeyboardModifiers.Shift;
+        if (metaState.HasFlag(MetaKeyStates.CtrlOn))
+            result |= KeyboardModifiers.Control;
+        if (metaState.HasFlag(MetaKeyStates.AltOn))
+            result |= KeyboardModifiers.Alt;
+        if (metaState.HasFlag(MetaKeyStates.MetaOn))
+            result |= KeyboardModifiers.Command;
+
+        return result;
+    }
+}
