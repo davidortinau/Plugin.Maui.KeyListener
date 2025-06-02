150d24e2bfc5dff588d46ef92a22ac1f1f8cb4fd
Jeremy Powell <jeremy@visionaid.com>
2025-06-02 12:25:59 +1200

    Connect PlatformBehavior to XamlRoot.Content

--- src/Plugin.Maui.KeyListener/KeyboardBehavior.Windows.cs
+++ src/Plugin.Maui.KeyListener/KeyboardBehavior.Windows.cs
@@ -1,73 +1,45 @@
 ﻿#if WINDOWS
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Plugin.Maui.KeyListener;
 
 public partial class KeyboardBehavior : PlatformBehavior<VisualElement>
 {
-	UIElement? _content;
-
 	protected override void OnAttachedTo(VisualElement bindable, FrameworkElement platformView)
 	{
 		base.OnAttachedTo(bindable, platformView);
 
-		var window = bindable.Window.Handler.PlatformView as Microsoft.UI.Xaml.Window;
-		if (bindable.Window?.Handler?.PlatformView is Microsoft.UI.Xaml.Window win && win.Content is UIElement content)
+		if (platformView.XamlRoot?.Content is FrameworkElement content)
 		{
-			_content = content;
-			_content.KeyDown += OnKeyDown;
-			_content.KeyUp += OnKeyUp;
-			_content.PreviewKeyDown += OnPreviewKeyDown;
-			_content.PreviewKeyUp += OnPreviewKeyUp;
+			content.KeyDown += OnKeyDown;
+			content.KeyUp += OnKeyUp;
 		}
 	}
 
 	protected override void OnDetachedFrom(VisualElement bindable, FrameworkElement platformView)
 	{
+		if (platformView.XamlRoot?.Content is FrameworkElement content)
+		{
+			content.KeyDown -= OnKeyDown;
+			content.KeyUp -= OnKeyUp;
+		}
+
 		base.OnDetachedFrom(bindable, platformView);
-
-		if (_content is null)
-			return;
-
-		_content.KeyDown -= OnKeyDown;
-		_content.KeyUp -= OnKeyUp;
-		_content.PreviewKeyDown -= OnPreviewKeyDown;
-		_content.PreviewKeyUp -= OnPreviewKeyUp;
-		_content = null;
 	}
 
-	void OnWindowKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
-	{
-		Console.WriteLine($"OnWindowKeyDown {e.Key}");
-	}
-
-	void OnKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+	void OnKeyDown(object sender, KeyRoutedEventArgs e)
 	{
 		var eventArgs = e.ToKeyPressedEventArgs();
-		this.RaiseKeyDown(eventArgs);
+		RaiseKeyDown(eventArgs);
 		if (eventArgs.Handled)
 			e.Handled = true;
 	}
-	void OnPreviewKeyUp(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
-	{
-		Console.WriteLine($"OnPreviewKeyUp {e.Key}");
-	}
 
-	void OnPreviewKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
-	{
-		Console.WriteLine($"OnPreviewKeyDown {e.Key}");
-	}
-
-	void OnKeyUp(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+	void OnKeyUp(object sender, KeyRoutedEventArgs e)
 	{
 		var eventArgs = e.ToKeyPressedEventArgs();
-		this.RaiseKeyUp(eventArgs);
+		RaiseKeyUp(eventArgs);
 		if (eventArgs.Handled)
 			e.Handled = true;
 	}
