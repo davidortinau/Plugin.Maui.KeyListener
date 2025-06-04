6477e4d948a207fce5012c79d7687a87b3145529
CraigBelser <craigbelser@gmail.com>
2025-06-04 12:20:42 -0500

    added sample code illistration of the scope issue

--- samples/Plugin.Maui.KeyListener.Sample/MainPage.xaml.cs
+++ samples/Plugin.Maui.KeyListener.Sample/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Plugin.Maui.KeyListener;
-
 namespace Plugin.Maui.KeyListener.Sample;
 
 public partial class MainPage : ContentPage
@@ -7,25 +6,29 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-		
-		AddKeyboardBehavior();
-	}
-
-	void AddKeyboardBehavior()
-	{
-		var keyboardBehavior = new KeyboardBehavior();
-		keyboardBehavior.KeyDown += (sender, args) => PrependOutput("KeyDown", args);
-		keyboardBehavior.KeyUp += (sender, args) => PrependOutput("KeyUp", args);
-		this.Behaviors.Add(keyboardBehavior);
-	}
-
-	void PrependOutput(string eventName, KeyPressedEventArgs e)
-	{
-		OutputLabel.Text = $"{eventName}: {e.Modifiers} {e.Keys} {e.KeyChar}" + Environment.NewLine + OutputLabel.Text;
 	}
 
 	void ClearButton_Clicked(object sender, EventArgs e)
 	{
-		OutputLabel.Text = string.Empty;
+		OutputLabel1.Text = string.Empty;
+		OutputLabel2.Text = string.Empty;
+		EntryTester1.Text = string.Empty;
+		EntryTester2.Text = string.Empty;
+	}
+
+	void OnKeyDown_Entry1(object sender, KeyPressedEventArgs e)
+	{
+		string newValue = $"KeyPressed= {e.Keys}, Modifiers={e.Modifiers}";
+		EntryTester1.Text = newValue;
+		OutputLabel1.Text = newValue + Environment.NewLine + OutputLabel1.Text;
+		e.Handled = true;
+	}
+
+	void OnKeyDown_Entry2(object sender, KeyPressedEventArgs e)
+	{
+		string newValue = $"KeyPressed= {e.Keys}, Modifiers={e.Modifiers}";
+		EntryTester2.Text = newValue;
+		OutputLabel2.Text = newValue + Environment.NewLine + OutputLabel2.Text;
+		e.Handled = true;
 	}
 }
