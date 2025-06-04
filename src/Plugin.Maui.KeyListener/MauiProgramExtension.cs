da14df36f0e536d5996aa2bd1a4e93526f63c0d8
Jeremy Powell <jeremy@visionaid.com>
2025-06-05 11:13:29 +1200

    Simplify ViewController registration

--- src/Plugin.Maui.KeyListener/MauiProgramExtension.cs
+++ src/Plugin.Maui.KeyListener/MauiProgramExtension.cs
@@ -1,24 +1,23 @@
-using Plugin.Maui.KeyListener;
+#if IOS || MACCATALYST
 using Microsoft.Maui.Handlers;
+#endif
 
 namespace Plugin.Maui.KeyListener;
+
 public static class MauiProgramExtensions
 {
 	public static MauiAppBuilder UseKeyListener(this MauiAppBuilder builder)
 	{
 		builder.ConfigureMauiHandlers(handlers =>
 		{
-			#if IOS || MACCATALYST
+#if IOS || MACCATALYST
 				PageHandler.PlatformViewFactory = (handler) =>
 				{
-					if (handler is not PageHandler pageHandler)
-						return null;
-						
-					var vc = new KeyboardPageViewController(handler.VirtualView, handler.MauiContext);
-					handler.ViewController = vc;
-					return (Microsoft.Maui.Platform.ContentView)vc.View.Subviews[0];
+					if (handler is PageHandler pageHandler)
+						handler.ViewController = new KeyboardPageViewController(handler.VirtualView, handler.MauiContext);
+					return null; // Fall through to default view creation
 				};
-			#endif
+#endif
 		});
 
 		return builder;
