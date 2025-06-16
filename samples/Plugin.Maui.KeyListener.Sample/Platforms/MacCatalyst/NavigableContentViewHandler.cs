9c8f207e6ad250a99b5865b871a9fd1178f5057a
CraigBelser <craigbelser@gmail.com>
2025-06-16 17:35:04 -0500

    More Macside contentview changes

--- samples/Plugin.Maui.KeyListener.Sample/Platforms/MacCatalyst/NavigableContentViewHandler.cs
+++ samples/Plugin.Maui.KeyListener.Sample/Platforms/MacCatalyst/NavigableContentViewHandler.cs
@@ -1,24 +1,27 @@
 ﻿using Microsoft.Maui.Handlers;
 using UIKit;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui;
 
 namespace Plugin.Maui.KeyListener.Sample;
 
 public class NavigableContentViewHandler : ViewHandler<ContentView, UIView>
 {
-	public NavigableContentViewHandler() : base(ViewHandler.ViewMapper)
+	public static IPropertyMapper<ContentView, NavigableContentViewHandler> Mapper =
+		new PropertyMapper<ContentView, NavigableContentViewHandler>(ViewHandler.ViewMapper);
+
+	public NavigableContentViewHandler() : base(Mapper)
 	{
 	}
 
 	protected override UIView CreatePlatformView()
 	{
-		return new FocusableContentUIView(); // Your custom UIView subclass
+		return new FocusableUIView();
 	}
 
 	protected override void ConnectHandler(UIView platformView)
 	{
 		base.ConnectHandler(platformView);
-
 		platformView.UserInteractionEnabled = true;
-		platformView.BecomeFirstResponder(); // Optional if you want auto-focus
 	}
 }
---
+++ src/Plugin.Maui.KeyListener/Platforms/MacCatalyst/FocusableUIView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace Plugin.Maui.KeyListener;
+
+public class FocusableUIView : UIView
+{
+	public override bool CanBecomeFocused => true;  
+	public override bool CanBecomeFirstResponder => true;  
+	public override bool CanResignFirstResponder => true;  
+
+	public FocusableUIView() : base()
+	{
+		Initialize();
+	}
+
+	public FocusableUIView(NSCoder coder) : base(coder)
+	{
+		Initialize();
+	}
+
+	public FocusableUIView(CGRect frame) : base(frame)
+	{
+		Initialize();
+	}
+
+	void Initialize()
+	{
+		UserInteractionEnabled = true;
+	}
+
+	public override void TouchesEnded(NSSet touches, UIEvent? evt)
+	{
+		this.BecomeFirstResponder();
+		base.TouchesEnded(touches, evt);
+	}
+}
