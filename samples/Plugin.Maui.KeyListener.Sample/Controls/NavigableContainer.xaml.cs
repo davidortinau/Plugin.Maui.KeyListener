89703d7b72ae7db53271aec1180cd70583295782
Belser, Jonathan <Jonathan.Belser@fmr.com>
2025-06-24 13:05:21 -0500

    added text doc

--- samples/Plugin.Maui.KeyListener.Sample/Controls/NavigableContainer.xaml.cs
+++ samples/Plugin.Maui.KeyListener.Sample/Controls/NavigableContainer.xaml.cs
@@ -19,7 +19,9 @@ public partial class NavigableContainer : ContentView
 	{
 		for (int i = 1; i < 5; i++)
 		{
-			NavContainer.Add(new NavigableContentView(){Text=$"Nav Control {i}"});
+			var item = new NavigableContentView() { Text = $"Nav Control {i}" };
+			SemanticProperties.SetDescription(item, item.Text);
+			NavContainer.Add(item);
 		}
 		Loaded -= OnLoaded;
 	}
